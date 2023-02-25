e6764a013183e1eaeb09129b4b740ba07bdc3f79
ivanoliva01 <ivan.olivalinares@alum.uca.es>
2023-02-25 10:05:32 +0100

    Actualizacion del Juego 25-02 Ivan
    Cambios/Suplementos
    -Movimiento de la bola
    -Destrucción del suelo
    -He añadido música de fondo, siempre y cuando se empiece el juego.
    -He añadido un sonido que se reproduce cada vez que se toca el espacio para cambiar la dirección de la bola.

--- DSH23_P2/Assets/Scripts/Movimiento.cs
+++ DSH23_P2/Assets/Scripts/Movimiento.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Movimiento : MonoBehaviour
 {
     public Camera cam;
-    public float velocidad;
     public GameObject prefabSuelo;
+    public int velocidad;
 
     Vector3 offset;
     //Rigidbody rb;
@@ -14,6 +14,10 @@ public class Movimiento : MonoBehaviour
     float valX;
     float valZ;
 
+
+    private Rigidbody rb;
+    private Vector3 direccionActual;
+    private AudioSource SonidoDeCambioDireccion;
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +27,9 @@ public class Movimiento : MonoBehaviour
 
         valX = 0.0f;
         valZ = 0.0f;
+        rb = GetComponent<Rigidbody>();
+        direccionActual = Vector3.forward;
+        SonidoDeCambioDireccion = GetComponent<AudioSource>();
         SueloInicial();
     }
 
@@ -39,6 +46,18 @@ public class Movimiento : MonoBehaviour
 
         // Mover la camara
         //cam.transform.position = this.transform.position + offset;
+        cam.transform.position = this.transform.position + offset; //para mover la camara con la bol
+        if(Input.GetKeyUp(KeyCode.Space)){
+            SonidoDeCambioDireccion.Play();
+            if(direccionActual == Vector3.forward)
+                direccionActual = Vector3.right;
+            else
+                direccionActual = Vector3.forward;
+        }
+
+        float tiempo = velocidad * Time.deltaTime;
+        rb.transform.Translate(direccionActual*tiempo);
+        
     }
 
     void SueloInicial()
@@ -48,5 +67,27 @@ public class Movimiento : MonoBehaviour
             valZ += 6.0f;
             GameObject elSuelo = Instantiate(prefabSuelo, new Vector3(valX, 0.0f, valZ), Quaternion.identity) as GameObject;
         }
+    }
+
+    void OnCollisionExit(Collision other) {
+        if(other.transform.tag == "Suelo"){
+            StartCoroutine(CrearSuelo(other));
+        }
+    }
+
+    IEnumerator CrearSuelo(Collision col){
+        yield return new WaitForSeconds(0.5f); //espera de 5 s
+        col.rigidbody.isKinematic = false;
+        col.rigidbody.useGravity = true;
+        yield return new WaitForSeconds(1f); //espera de 5 s
+        Destroy(col.gameObject); // se destruye el suelo
+        float ran = Random.Range(0f,1f); //creo un suelo nuevo de forma aleatoria, o hacia delante o hacia la derecha
+        if(ran < 0.5f)//creo un cubo hacia delante
+            valX += 6.0f;
+        else
+            valZ += 6.0f;
+        
+        //creo un cubo nuevo
+        GameObject elSuelo = Instantiate(prefabSuelo, new Vector3(valX, 0.0f, valZ), Quaternion.identity) as GameObject;
     }
 }
